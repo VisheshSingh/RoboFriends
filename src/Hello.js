5ce494ecb03d3874820808b89604d8a1ce38d588
Vishesh <visheshsingh16@gmail.com>
2018-12-07 17:17:38 -0500

    Creating Class based component and rendering it

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="f1 tc">
+        <h1>Hola Mundo!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt,
+          vero!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
