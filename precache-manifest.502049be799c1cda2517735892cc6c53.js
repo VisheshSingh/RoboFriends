d5ec796cc16b129cad605022399f0f5d796338b8
Vishesh <visheshsingh16@gmail.com>
2018-12-07 21:19:04 -0500

    Updates

---
+++ precache-manifest.502049be799c1cda2517735892cc6c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "ca3afec8294b0737b503",
+    "url": "/RoboFriends/static/js/main.ca3afec8.chunk.js"
+  },
+  {
+    "revision": "c65e17f7bb094e98ba2e",
+    "url": "/RoboFriends/static/js/1.c65e17f7.chunk.js"
+  },
+  {
+    "revision": "ca3afec8294b0737b503",
+    "url": "/RoboFriends/static/css/main.ddabe76c.chunk.css"
+  },
+  {
+    "revision": "c65e17f7bb094e98ba2e",
+    "url": "/RoboFriends/static/css/1.960e9924.chunk.css"
+  },
+  {
+    "revision": "620ad7d819fc58a2c81a2b7e43b46bb4",
+    "url": "/RoboFriends/index.html"
+  }
+];
