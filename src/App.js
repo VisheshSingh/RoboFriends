a3daa6eb6c30d1865419a34011f896b12c4f6bc7
Vishesh <visheshsingh16@gmail.com>
2018-12-07 19:31:20 -0500

    Using fetch api to request data from an external API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 
 class App extends Component {
   state = {
-    robots: robots,
+    robots: [],
     searchField: ""
   };
 
@@ -15,12 +14,25 @@ class App extends Component {
     });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => {
+        this.setState({
+          robots: users
+        });
+      });
+  }
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
+    if (this.state.robots.length === 0) {
+      return <h1>Fetching robots...</h1>;
+    }
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
