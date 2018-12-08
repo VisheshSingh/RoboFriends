b4640ff9ee012657d5dedba23e17f684a791d82b
Vishesh <visheshsingh16@gmail.com>
2018-12-07 19:10:50 -0500

    Updating the state using setState and passing it thru parent to sibling component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
 class App extends Component {
+  state = {
+    robots: robots,
+    searchField: ""
+  };
+
+  onSearchChange = e => {
+    this.setState({
+      searchField: e.target.value
+    });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = props => {
+  const { searchChange } = props;
   return (
     <div className="tc pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="Search for your robot..."
+        onChange={searchChange}
       />
     </div>
   );
